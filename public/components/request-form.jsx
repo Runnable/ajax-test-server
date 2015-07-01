ec8173074fb29bcd56d8e897452df1adf08b4d77
Tejesh Mehta <tejesh.mehta@gmail.com>
2015-07-01 14:20:47 -0700

    init

---
+++ public/components/request-form.jsx
@@ -0,0 +1,77 @@
+'use strict';
+
+var React = require('react');
+var formToObj = require('form-to-object');
+var $responses = document.getElementById('responses');
+var set = require('101/set');
+var clone = require('101/clone');
+var noop = require('101/noop');
+
+module.exports = RequestForm;
+
+function RequestForm () {
+  React.Component.apply(this, arguments);
+  this.onSubmit = this.props.onSubmit || noop;
+  this.state = {
+    formData: clone(this.props.formData || {}),
+    disabled: this.props.disabled
+  };
+}
+
+RequestForm.prototype = new React.Component();
+
+RequestForm.prototype.render = function () {
+  var disabled = this.state.disabled;
+  var formData = this.state.formData;
+
+  return <form onSubmit={ this.onSubmit }>
+    <div className="col-sm-6">
+      <div className="form-group">
+        <label for="method">Method</label>
+        <select className="form-control" value={ formData.method } defaultValue='GET' disabled={ disabled } required name="method">
+          <option value="POST"  >POST</option>
+          <option value="GET"   >GET</option>
+          <option value="PUT"   >PUT</option>
+          <option value="DELETE">DELETE</option>
+        </select>
+      </div>
+      <div className="form-group">
+        <label for="url">URL</label>
+        <input className="form-control" disabled={ disabled } type="text" name="url" value={ formData.url } />
+      </div>
+      <div className="form-group">
+        <label for="body">Body</label>
+        <textarea value={ formData.body } className="form-control" disabled={ disabled } rows="4" cols="50" name="body">
+        </textarea>
+      </div>
+    </div>
+    <div className="col-sm-6">
+      <div className="form-group">
+        <label for="headers">Headers</label>
+        <textarea value={ formData.headers } className="form-control" disabled={ disabled } rows="4" cols="50" name="headers">
+        </textarea>
+      </div>
+      <div className="checkbox">
+        <label>
+          <input type="checkbox" name="withCredentials" checked={ formData.withCredentials }>withCredentials</input>
+        </label>
+      </div>
+      <div className="form-group">
+        <label for="numRequests">NumRequests</label>
+        <select className="form-control" value={ formData.numRequests } defaultValue='1' disabled={ disabled } required name="numRequests">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
+      </div>
+      <button className={ disabled ? 'hide' : '' } type="submit">Submit</button>
+    </div>
+  </form>;
+
+  function isSelected (key, compare, isDefault) {
+    var val = formData[key];
+    return val ? (val === compare) : isDefault;
+  }
+};
---
+++ public/components/response-data.jsx
@@ -0,0 +1,73 @@
+'use strict';
+
+var React = require('react');
+
+module.exports = ResponseData;
+
+function ResponseData () {
+  React.Component.apply(this, arguments);
+  this.state = {};
+  var xhr = this.createXHR(this.props.req);
+}
+
+ResponseData.prototype = new React.Component();
+
+ResponseData.prototype.createXHR = function (req) {
+  var data = req.data;
+  this.state = {};
+  if (!data) { return; }
+  var xhr = this.xhr = new XMLHttpRequest();
+  // xhr handlers
+  xhr.onload = this.handleSuccess.bind(this);
+  xhr.ontimeout = this.handleTimeout.bind(this);
+  xhr.onerror = this.handleError.bind(this);
+  // send xhr
+  try {
+    xhr.open(data.method, data.url, true);
+    xhr.withCredentials = data.withCredentials;
+    data.headers.split(/\n/g).forEach(function (headerKeyVal) {
+      var split = headerKeyVal.split('=');
+      var key = split[0] && split[0].trim();
+      var val = split[1] && split[1].trim();
+      if (key) {
+        xhr.setRequestHeader(key, val);
+      }
+    });
+    xhr.send(data.body);
+  }
+  catch (e) {
+    this.handleError(e);
+  }
+};
+
+ResponseData.prototype.handleSuccess = function () {
+  if (this.state.data != null) { return; }
+  var xhr = this.xhr;
+  this.setState({
+    body: xhr.responseText
+  });
+};
+
+ResponseData.prototype.handleTimeout = function () {
+  this.setState({
+    body: 'TIMEOUT:TIMEOUT:TIMEOUT:TIMEOUT:TIMEOUT'
+  });
+};
+
+ResponseData.prototype.handleError = function (err) {
+  window.hey = this;
+  var self = this;
+  this.setState({
+    body: err.stack || 'Possibly CORS Error'
+  });
+};
+
+ResponseData.prototype.render = function () {
+  return <form>
+    <div className="form-group">
+      <label for="response">Response</label>
+      <textarea value={ this.state.body } className="form-control" disabled={ true } rows="2" cols="50" name="response">
+      </textarea>
+    </div>
+  </form>;
+};
